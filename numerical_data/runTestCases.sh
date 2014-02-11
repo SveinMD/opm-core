64ce5a3ae36b8b01176461b4884e810a4e1b472e
Svein Morten Drejer <svein.morten.drejer@gmail.com>
2014-02-11 12:38:25 +0100

    Added simulation options to runTestCases.sh

--- numerical_data/runTestCases.sh
+++ numerical_data/runTestCases.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
+
+float_scale=2
+
+function float_eval()
+{
+    local stat=0
+    local result=0.0
+    if [[ $# -gt 0 ]]; then
+        result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
+        stat=$?
+        if [[ $stat -eq 0 && -z "$result" ]]; then stat=1; fi
+    fi
+    echo $result
+    return $stat 
+}
+
 file="testCase"
 ext=".out"
 outfile=$file$ext
-sum=0
+e="0"
+
+echo "Solver [r/t/i/b/n + return]:"
+read solver
+
+echo "Print [-p/<blank> + return]:"
+read do_print
+
+echo "**Running solvers**"
+echo "..."
+
 for i in {1..10}
 do
  #outfile=$file$i$ext
  #../bin/testCase1 -p -s b &> $outfile
- ../bin/testCase1 -s r &> $outfile
+ ../bin/testCase1 $do_print -s $solver &> $outfile
  resultFull=$(tail -1 $outfile | head -1)
  result=$(echo $resultFull java | cut -d ' ' -f4)
- sum=$sum+$result
+ e=$e" + "$result
 done
-echo $sum
+echo "Test run in $(float_eval "$e") seconds"
