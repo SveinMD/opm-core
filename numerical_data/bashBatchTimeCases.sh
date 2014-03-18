4de341270e8b7eb3b6de31079d152c308ce2edfb
Svein Morten Drejer <svein.morten.drejer@gmail.com>
2014-03-18 14:04:48 +0100

    Added uniform source terms to .sh file with all run commands for time cases

--- numerical_data/bashBatchTimeCases.sh
+++ numerical_data/bashBatchTimeCases.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1 -m 1 1 -s r;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1 -m 1 1 -s t;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1 -m 1 1 -s t a;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1 -m 1 1 -s b;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1 -m 1 1 -s i;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1 -m 1 1 -s r;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1 -m 1 1 -s t;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1 -m 1 1 -s t a;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1 -m 1 1 -s b;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1 -m 1 1 -s i;
 
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10 -m 1 1 -s r;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10 -m 1 1 -s t;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10 -m 1 1 -s t a;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10 -m 1 1 -s b;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10 -m 1 1 -s i;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10 -m 1 1 -s r;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10 -m 1 1 -s t;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10 -m 1 1 -s t a;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10 -m 1 1 -s b;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10 -m 1 1 -s i;
 
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 100 -m 1 1 -s r;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 100 -m 1 1 -s t;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 100 -m 1 1 -s t a;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 100 -m 1 1 -s b;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 100 -m 1 1 -s i;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 100 -m 1 1 -s r;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 100 -m 1 1 -s t;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 100 -m 1 1 -s t a;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 100 -m 1 1 -s b;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 100 -m 1 1 -s i;
 
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1000 -m 1 1 -s r;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1000 -m 1 1 -s t;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1000 -m 1 1 -s t a;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1000 -m 1 1 -s b;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 1000 -m 1 1 -s i;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1000 -m 1 1 -s r;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1000 -m 1 1 -s t;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1000 -m 1 1 -s t a;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1000 -m 1 1 -s b;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 1000 -m 1 1 -s i;
 
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10000 -m 1 1 -s r;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10000 -m 1 1 -s t;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10000 -m 1 1 -s t a;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10000 -m 1 1 -s b;
-./runTimeStepCases.py -T 2000 --dim 120 120 20 20 --perm h 10000 -m 1 1 -s i;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10000 -m 1 1 -s r;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10000 -m 1 1 -s t;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10000 -m 1 1 -s t a;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10000 -m 1 1 -s b;
+./runTimeStepCases.py -T 2000 --dim 120 120 20 20 -i 180 --perm h 10000 -m 1 1 -s i;
 
