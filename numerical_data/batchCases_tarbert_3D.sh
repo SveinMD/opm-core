76d2cce93cfeacfcd4649a2e83ec30507683e8db
Svein Morten Drejer <svein.morten.drejer@gmail.com>
2014-06-01 14:40:42 +0200

    Added batch files for the tarbert and upper ness layer cases

---
+++ numerical_data/batchCases_tarbert_3D.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s r;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s t;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s a;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s b;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s i;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 10 -s g;
+
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s r;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s t;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s a;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s b;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s i;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 1 1 -s g;
+
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s r;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s t;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s a;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s b;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s i;
+nice ./runTimeStepCases_short.py -f ../bin/spe10 -T 30 -n 20 --dim 60 60 20 60 110 20 -i 50 --perm i 0 0 0 -m 10 1 -s g;
